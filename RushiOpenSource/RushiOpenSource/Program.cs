5eccefac6a38191f2a41e167646396995393e532
HammadMaqbool <hammad.mqbl@gmail.com>
2025-03-21 20:40:11 -0700

    Addition program

--- RushiOpenSource/RushiOpenSource/Program.cs
+++ RushiOpenSource/RushiOpenSource/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int valueone;
+int valuetwo;
+
+Console.WriteLine("Please enter value one");
+valueone = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter value two");
+valuetwo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("The sum of two values is: " + (valueone + valuetwo));
